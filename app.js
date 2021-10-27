e7b78fcdf69978188abe9c437885b6a30e34c4f9
Karolina <karolinakotwica93@gmail.com>
2021-10-27 19:52:23 +0200

    Added project

--- app.js
+++ app.js
@@ -627,6 +627,8 @@ app.get("/edit/:id", (req, res) => {
     })
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started on port 3000");
-} )
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
