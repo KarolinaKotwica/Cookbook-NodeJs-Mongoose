9d5104d1fd2d590e2df4af54eb351f31e6f615b6
Karolina <karolinakotwica93@gmail.com>
2021-10-31 19:06:27 +0100

    Update google login

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "http://book-to-cook.herokuapp.com/auth/google/callback",
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
