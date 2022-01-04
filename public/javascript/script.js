ce78341d2ee4422ea2d1c5910dbcecf0dbc28fb8
Karolina <karolinakotwica93@gmail.com>
2022-01-04 15:21:00 +0100

    Update app

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -21,6 +21,7 @@ if(window.sessionStorage) {
   })
 }
 
+// Tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
