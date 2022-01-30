05d2a33fb508375a0689c1c14d79891919cdbf43
Karolina <karolinakotwica93@gmail.com>
2022-01-30 13:07:13 +0100

    update app

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -96,3 +96,9 @@ loadList();
 
 window.onload = load;
 ////// end pages //////
+
+
+// google translate
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'pl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
