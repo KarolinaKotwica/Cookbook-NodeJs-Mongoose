157d04ac2ca14cdcd36c3ca7bd0a29c3576464e9
Karolina <karolina.kotwica@powercoders.org>
2023-07-02 15:25:16 +0200

    Remove google translate

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -113,6 +113,6 @@ window.onload = load;
 
 
 // google translate
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'pl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-}
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement({pageLanguage: 'pl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+// }
